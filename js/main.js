c14d54425bb405ff2ab1660265b2a1629a23d035
Wesley Pickering <wesley.pickering@berkeley.edu>
2017-01-03 17:09:06 -0800

    Added tux to 0,0

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 var game = new Phaser.Game(1080, 720, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
+  game.load.image('tux', 'res/tux.png');
 }
 
 function create() {
+  players = game.add.group();
+  createPlayer();
+}
+
+function createPlayer() {
+  var player = players.create(0, 0, 'tux');
 }
 
 function update() {
-}
-
-function createPlayer(x,y){
-}
-
-function playerUpdate(){
-}
-
-function createPlatform(){
 }
