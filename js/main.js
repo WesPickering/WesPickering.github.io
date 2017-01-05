c52e7e3bd1e5c404b39ab16d89b8b5db84c8a99c
WesPickering <wpickering888@gmail.com>
2017-01-05 02:50:41 -0800

    Update main.js
    modified size of video game.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(1080, 720, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(940, 660, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var cursors;
 
 function preload() {
