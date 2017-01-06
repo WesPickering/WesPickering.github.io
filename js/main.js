5b03b6380d1f919518f60be9e83e8855f96cd7a3
Wesley Pickering <wesley.pickering@berkeley.edu>
2017-01-05 17:32:01 -0800

    Added floor Variables and immovability to tiles

--- js/main.js
+++ js/main.js
@@ -1,37 +1,53 @@
-var game = new Phaser.Game(940, 660, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(720, 660, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var cursors;
 
 function preload() {
   game.load.image('tux', 'res/tux.png');
+  game.load.image('brick', 'res/brick.png');
 }
 
 function create() {
   game.stage.backgroundColor = "#4488AA";
   game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  //players
   players = game.add.group();
   players.enableBody = true;
-  
+  createPlayer(10, 10);
+
+  //controls
   cursors = game.input.keyboard.createCursorKeys();
 
-  createPlayer(10, 10);
+  //floor
+  platforms = game.add.group();
+  createPlatform();
 }
 
-function createPlayer(x, y ) {
+function createPlayer(x, y) {
   var player = players.create(x, y, 'tux');
   player.body.bounce.y = 0.2;
   player.body.gravity.y = 300;
   player.body.collideWorldBounds =  true;
 }
 
+function createPlatform() {
+  for(var i = 0; i < game.world.width; i += 64) {
+    var ground = platforms.create(i, game.world.height, 'brick');
+    ground.body.immovable = true;
+  }
+}
+
 function update() {
   game.physics.arcade.collide(players, players);
+  game.physics.arcade.collide(players, platforms);
+  game.physics.arcade
   players.forEach(function(p){
     p.body.velocity.x = 0;
     if(cursors.left.isDown) {
       p.body.velocity.x = -150;
     }
     else if(cursors.right.isDown) {
-      p.body.velocity.x = 150; 
+      p.body.velocity.x = 150;
     }
   });
 }
