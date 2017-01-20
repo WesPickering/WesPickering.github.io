699f1a8e99756fb9f4142179d98aa1d83a7fa643
Wesley Pickering <wesley.pickering@berkeley.edu>
2017-01-19 20:40:26 -0800

    added controls in update function

--- js/shooter.js
+++ js/shooter.js
@@ -16,11 +16,11 @@ function create() {
   //players
   player1 = game.add.group();
   player1.enableBody = true;
-  createPlayer1(10, game.world.height - 200, 200, 400);
+  createPlayer1();
 
   player2 = game.add.group();
   player2.enableBody = true;
-  createPlayer2()
+  createPlayer2();
 
   //controls
   cursors = game.input.keyboard.createCursorKeys();
@@ -33,20 +33,43 @@ function create() {
 
 }
 
-function createPlayer1(x, y) {
+function createPlayer1() {
   var p1 = player1.create(15, game.world.height / 2, 'red');
   p1.body.collideWorldBounds = true;
   p1.speed = speed;
 }
 
-function createPlayer2(x, y) {
-  var p2 = player2.create(game.world.width - 15, game.world.height / 2, 'blue');
+function createPlayer2() {
+  var p2 = player2.create(game.world.width - 75, game.world.height / 2, 'blue');
   p2.body.collideWorldBounds = true;
   p2.speed = speed;
 }
 
   function update() {
     game.physics.arcade.collide(player1, player2);
+
+//Player 1 controls
+    if (move_up.isDown) {
+      player1.body.velocity.y = -speed;
+    } if (move_down.isDown) {
+      player1.body.velocity.y = speed;
+    } if (move_left.isDown) {
+      player1.body.velocity.x = -speed;
+    } if (move_right.isDown) {
+      player1.body.velocity.x = speed;
+    } if (shoot_1.isDown) {
+    }
+
+//Player 2 Controls
+    if (cursors.up.isDown) {
+      player2.body.velocity.y = -speed;
+    } if (cursors.down.isDown) {
+      player2.body.velocity.y = speed;
+    } if (cursors.left.isDown) {
+      player2.body.velocity.x = -speed;
+    } if (cursors.right.isDown) {
+      player2.body.velocity.x = speed;
+    }
 
 
 
